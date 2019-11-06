d411536a5254580ebfafdd4313027c5cff29353e
swati <swats.gautam@gmail.com>
2019-11-06 17:24:05 +0530

    script updated

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ Promise.all([
 ]).then(startVideo)
 
 function startVideo() {
-  navigator.getUserMedia(
+  navigator.mediaDevices.getUserMedia(
     { video: {} },
     stream => {
       video.srcObject = stream
@@ -17,17 +17,23 @@ function startVideo() {
   )
 }
 
-// video.addEventListener('play', () => {
-//   const canvas = faceapi.createCanvasFromMedia(video)
-//   document.body.append(canvas)
-//   const displaySize = { width: video.width, height: video.height }
-//   faceapi.matchDimensions(canvas, displaySize)
-//   // setInterval(async () => {
-//   //   const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-//   //   const resizedDetections = faceapi.resizeResults(detections, displaySize)
-//   //   canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-//   //   faceapi.draw.drawDetections(canvas, resizedDetections)
-//   //   faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-//   //   faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-//   // }, 100)
-// })
+startVideo()
+
+video.addEventListener('play', () => {
+  //create the canvas from video element as we have created above
+  const canvas = faceapi.createCanvasFromMedia(video);
+  //append canvas to body or the dom element where you want to append it
+  document.body.append(canvas)
+  // displaySize will help us to match the dimension with video screen and accordingly it will draw our detections
+  // on the streaming video screen
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
+})
